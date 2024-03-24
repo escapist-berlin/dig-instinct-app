b610e4fe943f9ce00471c2c2be5594d4a5be6c7c
Denis Kolokolov <dennis.kolokolov@gmail.com>
2024-03-24 19:45:54 +0100

    [add] Release: model, controller, migration & route

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -32,6 +32,9 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+                                <NavLink :href="route('releases.index')" :active="route().current('releases.index')">
+                                  Releases
+                              </NavLink>
                             </div>
                         </div>
 
@@ -114,6 +117,9 @@ const showingNavigationDropdown = ref(false);
                     <div class="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('releases.index')" :active="route().current('releases.index')">
+                          Releases
                         </ResponsiveNavLink>
                     </div>
 
---
+++ resources/js/Pages/Releases/Index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { useForm, Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+  <Head title="Releases" />
+
+  <AuthenticatedLayout>
+    <div>
+      More than 2000 discogs releases ...
+    </div>
+  </AuthenticatedLayout>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
