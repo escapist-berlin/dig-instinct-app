9ef48444af634004e59a69cb08c7e04a46203efe
Denis Kolokolov <dennis.kolokolov@gmail.com>
2024-04-12 17:18:56 +0200

    Create Track model and initial tracks table migration

---
+++ app/Models/Track.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Track extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'duration',
+        'position',
+    ];
+
+    public function release()
+    {
+        return $this->belongsTo(Release::class);
+    }
+
+    public function artists()
+    {
+        return $this->belongsToMany(Artist::class, 'artist_track');
+    }
+}
